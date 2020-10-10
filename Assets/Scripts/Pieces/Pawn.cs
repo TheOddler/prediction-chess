abee29358e93cc89b2814ffa24264f7015a08763
Pablo <pablo.bollansee@gmail.com>
2020-10-10 14:24:18 +0200

    👷‍♂️ Renaming for clarity

--- Assets/Scripts/Pieces/Pawn.cs
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -4,14 +4,14 @@ public class Pawn : Piece
 {
     public override HashSet<BoardPosition> CalculateLegalDestinations()
     {
-        var legalMoves = new HashSet<BoardPosition>();
+        var legalDestinations = new HashSet<BoardPosition>();
         int dir = Color == ChessColor.White ? 1 : -1;
 
         // Move
         BoardPosition normalMove = Position.Add(0, dir);
         if (Others.AtPosition(normalMove) == null)
         {
-            legalMoves.Add(normalMove);
+            legalDestinations.Add(normalMove);
         }
 
         // Double move
@@ -19,7 +19,7 @@ public class Pawn : Piece
         bool doubleMoveAllowed = (Color == ChessColor.White && Position.y == 1) || (Color == ChessColor.Black && Position.y == 6);
         if (doubleMoveAllowed && Others.AtPosition(doubleMove) == null)
         {
-            legalMoves.Add(doubleMove);
+            legalDestinations.Add(doubleMove);
         }
 
         // Attack
@@ -27,16 +27,16 @@ public class Pawn : Piece
         BoardPosition attack2 = Position.Add(-1, dir);
         if (Enemies.AtPosition(attack1) != null)
         {
-            legalMoves.Add(attack1);
+            legalDestinations.Add(attack1);
         }
         if (Enemies.AtPosition(attack2) != null)
         {
-            legalMoves.Add(attack2);
+            legalDestinations.Add(attack2);
         }
 
         // Cleanup (it's possible when we are at the edge we may have added our own position as a valid move, remove that)
-        legalMoves.Remove(Position);
+        legalDestinations.Remove(Position);
 
-        return legalMoves;
+        return legalDestinations;
     }
 }
