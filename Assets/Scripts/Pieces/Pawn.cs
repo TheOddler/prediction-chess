9737f8405c422d3bdd0a291b3e236b96b22338b1
Pablo <pablo.bollansee@gmail.com>
2020-10-10 14:06:04 +0200

    🧹 Cleanup

--- Assets/Scripts/Pieces/Pawn.cs
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -5,20 +5,18 @@ public class Pawn : Piece
     public override HashSet<BoardPosition> CalculateLegalDestinations()
     {
         var legalMoves = new HashSet<BoardPosition>();
-        var others = OtherPieces;
-        var enemies = Enemies;
         int dir = Color == ChessColor.White ? 1 : -1;
 
         // Move
         BoardPosition normalMove = Position.Add(0, dir);
-        if (others.AtPosition(normalMove) == null)
+        if (Friends.AtPosition(normalMove) == null)
         {
             legalMoves.Add(normalMove);
         }
 
         // Double move
         BoardPosition doubleMove = Position.Add(0, dir * 2);
-        if ((Color == ChessColor.White && Position.y == 1 || Color == ChessColor.Black && Position.y == 6) && others.AtPosition(doubleMove) == null)
+        if ((Color == ChessColor.White && Position.y == 1 || Color == ChessColor.Black && Position.y == 6) && Friends.AtPosition(doubleMove) == null)
         {
             legalMoves.Add(doubleMove);
         }
@@ -26,11 +24,11 @@ public class Pawn : Piece
         // Attack
         BoardPosition attack1 = Position.Add(1, dir);
         BoardPosition attack2 = Position.Add(-1, dir);
-        if (enemies.AtPosition(attack1) != null)
+        if (Enemies.AtPosition(attack1) != null)
         {
             legalMoves.Add(attack1);
         }
-        if (enemies.AtPosition(attack2) != null)
+        if (Enemies.AtPosition(attack2) != null)
         {
             legalMoves.Add(attack2);
         }
